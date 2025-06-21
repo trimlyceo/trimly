42916ce3491da7841ffd4d56ab9cc51c3d271f69
trimlyceo <hapiihoto@icloud.com>
2025-06-21 04:01:32 +0200

    Connect controller to Booking model

--- server/routes/server/controllers/bookingController.js
+++ server/routes/server/controllers/bookingController.js
@@ -1,9 +1,12 @@
+const { addBooking, getBookings } = require('../models/Booking');
+
 exports.createBooking = (req, res) => {
-  const booking = req.body;
-  console.log('New booking received:', booking);
+  const booking = addBooking(req.body);
+  console.log('New booking:', booking);
   res.status(201).json({ message: 'Booking created', booking });
 };
 
 exports.getBookings = (req, res) => {
-  res.status(200).json({ message: 'Bookings fetched', bookings: [] });
+  const bookings = getBookings();
+  res.status(200).json({ bookings });
 };
